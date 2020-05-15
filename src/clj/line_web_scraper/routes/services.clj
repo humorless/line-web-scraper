c94507cefa1a14166b84384ec23d9bcde4bd7fa2
Laurence Chen <laurence@replware.dev>
2020-05-15 14:11:24 +0800

    remove web API - /api/math/plus

--- src/clj/line_web_scraper/routes/services.clj
+++ src/clj/line_web_scraper/routes/services.clj
@@ -60,23 +60,6 @@
                         {:status 200
                          :body (scrape/check-lp-tag url)})}}]]
 
-   ["/math"
-    {:swagger {:tags ["math"]}}
-
-    ["/plus"
-     {:get {:summary "plus with spec query parameters"
-            :parameters {:query {:x int?, :y int?}}
-            :responses {200 {:body {:total pos-int?}}}
-            :handler (fn [{{{:keys [x y]} :query} :parameters}]
-                       {:status 200
-                        :body {:total (+ x y)}})}
-      :post {:summary "plus with spec body parameters"
-             :parameters {:body {:x int?, :y int?}}
-             :responses {200 {:body {:total pos-int?}}}
-             :handler (fn [{{{:keys [x y]} :body} :parameters}]
-                        {:status 200
-                         :body {:total (+ x y)}})}}]]
-
    ["/files"
     {:swagger {:tags ["files"]}}
 
